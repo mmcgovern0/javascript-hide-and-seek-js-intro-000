6de684c01f1e2f0b8cd2fedd104474392e91eafe
Michael McGovern <mmcgovern.wework@gmail.com>
2019-02-26 19:34:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function getFirstSelector(selector) {
 function nestedTarget(){
   return document.querySelector('#nested .target');
 }
+
+function increaseRankBy(n) {
+  const rankedList = document.querySelectorAll('ranked-list');
+
+  for (let i=0; i < ranked-list.length; i++) {
+    let children = ranked-list[i].children;
+
+    for (let j=0; j<children.length; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
